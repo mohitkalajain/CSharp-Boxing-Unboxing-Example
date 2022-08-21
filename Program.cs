b24b9f7653a9d3b6b05b1af74acd659def591f4a
Mohit Kala <73868188+mohitkalajain@users.noreply.github.com>
2022-08-21 08:57:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int abc = 5000;
+object obj = abc;//Boxing
+int xyz = (int)obj;//UnBoxing
+Console.WriteLine("Value Of Object = "+ abc);
+Console.WriteLine("Value Of Xyz = " + xyz);
